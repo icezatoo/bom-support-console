0278009a8939947b706d6683fb844def7b1c2344
Watcharapong <icetruedigital@gmail.com>
2021-04-09 09:37:56 +0700

    initial commit

---
+++ apps/bom-support-console-web/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'bom-support-console-web',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/bom-support-console-web',
+};
